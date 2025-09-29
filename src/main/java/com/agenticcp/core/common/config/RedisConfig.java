2e63490c0ada909fee7288ae8ced2f196909bfdb
yejinj <sitting077@gmail.com>
2025-09-29 22:58:39 +0900

    fix: RedisConfig 충돌 정리 - develop 기준으로 통일 (#48)

--- src/main/java/com/agenticcp/core/common/config/RedisConfig.java
+++ src/main/java/com/agenticcp/core/common/config/RedisConfig.java
@@ -1,42 +1,37 @@
 package com.agenticcp.core.common.config;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.beans.factory.annotation.Value;
 
+/**
+ * Redis 설정 클래스
+ */
 @Configuration
-@Slf4j
 public class RedisConfig {
 
-    @Value("${redis.host:localhost}")
-    private String redisHost;
-
-    @Value("${redis.port:6379}")
-    private int redisPort;
-
+    /**
+     * RedisTemplate 빈 설정
+     * 
+     * @param connectionFactory Redis 연결 팩토리
+     * @return RedisTemplate
+     */
     @Bean
-    @ConditionalOnProperty(name = "redis.enabled", havingValue = "true", matchIfMissing = true)
-    public RedisConnectionFactory redisConnectionFactory() {
-        log.info("Redis 연결 팩토리 생성: {}:{}", redisHost, redisPort);
-        return new LettuceConnectionFactory(redisHost, redisPort);
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "redis.enabled", havingValue = "true", matchIfMissing = true)
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        log.info("RedisTemplate 빈 생성");
-        RedisTemplate<String, String> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        // Key 직렬화 설정
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
+
+        // Value 직렬화 설정
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         template.afterPropertiesSet();
         return template;
     }
