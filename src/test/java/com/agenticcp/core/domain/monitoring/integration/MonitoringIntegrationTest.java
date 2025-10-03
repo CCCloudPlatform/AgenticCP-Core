d70a6d3700e6a63641621dbc5d38563780a5f9ed
grovince <grovinceln@gmail.com>
2025-10-03 20:07:10 +0900

    fix: 데이터베이스 및 통합 테스트 이슈 해결
    - DatabaseHealthIndicator 구현 수정
    - MonitoringIntegrationTest 활성화 (@Disabled 제거)
    - H2 데이터베이스 통합 및 엔티티 매핑 문제 해결

--- src/test/java/com/agenticcp/core/domain/monitoring/integration/MonitoringIntegrationTest.java
+++ src/test/java/com/agenticcp/core/domain/monitoring/integration/MonitoringIntegrationTest.java
@@ -4,6 +4,7 @@ import com.agenticcp.core.domain.monitoring.entity.Metric;
 import com.agenticcp.core.domain.monitoring.repository.MetricRepository;
 import com.agenticcp.core.domain.monitoring.service.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
