5210fe0e050d233a9a6e438bcfa2d8db08793556
YuSung011017 <salamander011017@gmail.com>
2025-10-18 14:31:29 +0900

    fix: SecurityInspectorController ApiResponse import 경로 수정
    - ApiResponse import를 올바른 패키지 경로로 수정
    - 컴파일 에러 해결로 Issue #45 테스트 실행 가능

--- src/main/java/com/agenticcp/core/domain/security/controller/SecurityInspectorController.java
+++ src/main/java/com/agenticcp/core/domain/security/controller/SecurityInspectorController.java
@@ -1,6 +1,6 @@
 package com.agenticcp.core.domain.security.controller;
 
-import com.agenticcp.core.common.dto.ApiResponse;
+import com.agenticcp.core.common.dto.exception.ApiResponse;
 import com.agenticcp.core.domain.security.annotation.RequirePermission;
 import com.agenticcp.core.domain.security.annotation.RequireRole;
 import com.agenticcp.core.domain.security.service.AuthorizationService;
