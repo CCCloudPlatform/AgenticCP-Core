b23df570bf9953f59952c46bcbcbcd9f084212a9
yeonsoo <dfh56541@gmail.com>
2025-10-25 03:02:48 +0900

    feat: VpcUseCaseService

---
+++ src/main/java/com/agenticcp/core/domain/cloud/service/aws/VpcUseCaseService.java
@@ -0,0 +1,29 @@
+package com.agenticcp.core.domain.cloud.service.aws;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.agenticcp.core.domain.cloud.entity.CloudResource;
+import com.agenticcp.core.domain.cloud.port.model.VpcCreateRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class VpcUseCaseService {
+
+    private final VpcPortRouter vpcPortRouter;
+
+    @Transactional
+    public CloudResource createVpc(VpcCreateRequest request) {
+    
+    // 포트 선택 
+    VpcManagementPort vpcPort = vpcPortRouter.getPort(request.getProviderType());
+
+    // VPC 생성 
+    CloudResource vpc = vpcPort.createVpc(request);
+    
+    return vpc;
+    } 
+}
