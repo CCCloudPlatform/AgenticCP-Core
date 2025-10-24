8e0e05ff288a8e710ccfa9d4f1b234e7488e25a4
yeonsoo <dfh56541@gmail.com>
2025-10-24 16:23:01 +0900

    feat: SharedIsolationStrategy 구현체

---
+++ src/main/java/com/agenticcp/core/domain/tenant/cloud/service/SharedIsolationStrategy.java
@@ -0,0 +1,40 @@
+package com.agenticcp.core.domain.tenant.cloud.service;
+
+import com.agenticcp.core.domain.tenant.adapter.dto.IsolationResult;
+import com.agenticcp.core.domain.tenant.cloud.dto.CloudResourceResult;
+import com.agenticcp.core.domain.tenant.cloud.dto.ResourceCreateRequest;
+import com.agenticcp.core.domain.tenant.entity.TenantIsolation;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class SharedIsolationStrategy implements IsolationStrategy{
+
+    @Override
+    public boolean supports(TenantIsolation.IsolationLevel isolationLevel) {
+        return isolationLevel == TenantIsolation.IsolationLevel.SHARED;
+    }
+
+    @Override
+    public IsolationResult applyIsolation(String tenantKey, TenantIsolation.IsolationLevel isolationLevel, CloudResourceCreator resourceCreator) {
+
+        // VPC 요청 생성
+        ResourceCreateRequest vpcRequest = ResourceCreateRequest.builder()
+                .metadata(Map.of(
+                        "tenantKey", tenantKey,
+                        "isolationLevel", "SHARED"))
+                .build();
+
+        // ResourceCreator 에게 VPC 리소스 생성 위임
+        CloudResourceResult vpcResult = resourceCreator.createVpc(tenantKey, vpcRequest, isolationLevel.SHARED);
+
+        // 결과값(TODO: subnet, sg 생성 결과 포함)
+        Map<String, Object> resourceDetails = Map.of(
+                "vpcId", vpcResult.resourceId()
+        );
+
+        return IsolationResult.success(tenantKey, isolationLevel, resourceDetails);
+
+    }
+}
