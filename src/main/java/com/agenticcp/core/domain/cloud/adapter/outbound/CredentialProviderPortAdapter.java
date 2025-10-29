17de22c4058957f0fdf14eb5b5d5728127754c47
syeongkim <carsk0607@gmail.com>
2025-10-29 21:22:54 +0900

    feat: CredentialProviderPortAdapter 추가

---
+++ src/main/java/com/agenticcp/core/domain/cloud/adapter/outbound/CredentialProviderPortAdapter.java
@@ -0,0 +1,87 @@
+package com.agenticcp.core.domain.cloud.adapter.outbound;
+
+import com.agenticcp.core.common.exception.BusinessException;
+import com.agenticcp.core.domain.cloud.entity.CloudProvider.ProviderType;
+import com.agenticcp.core.domain.cloud.exception.CloudErrorCode;
+import com.agenticcp.core.domain.cloud.port.outbound.CredentialProviderPort;
+import com.agenticcp.core.domain.cloud.service.AwsCredentialManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 자격증명 제공 포트의 실제 구현체
+ * 프로바이더 타입에 따라 적절한 자격증명 관리자에게 위임합니다.
+ * 
+ * @author AgenticCP Team
+ * @version 1.0.0
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CredentialProviderPortAdapter implements CredentialProviderPort {
+
+    private final AwsCredentialManager awsCredentialManager;
+
+    @Override
+    public Object resolveCredentials(String tenantKey, ProviderType providerType, String accountScope) {
+        log.debug("[CredentialProviderPortAdapter] resolveCredentials - tenantKey={}, providerType={}, accountScope={}",
+                tenantKey, providerType, accountScope);
+
+        switch (providerType) {
+            case AWS:
+                return awsCredentialManager.getCredentials(accountScope);
+            case AZURE:
+            case GCP:
+                throw new BusinessException(
+                    CloudErrorCode.UNSUPPORTED_OPERATION,
+                    String.format("프로바이더 타입 %s는 아직 지원되지 않습니다", providerType)
+                );
+            default:
+                throw new BusinessException(
+                    CloudErrorCode.UNSUPPORTED_OPERATION,
+                    String.format("알 수 없는 프로바이더 타입입니다: %s", providerType)
+                );
+        }
+    }
+
+    @Override
+    public String storeCredentials(String tenantKey, ProviderType providerType, 
+                                   String accountScope, Map<String, String> credentials) {
+        log.debug("[CredentialProviderPortAdapter] storeCredentials - tenantKey={}, providerType={}, accountScope={}",
+                tenantKey, providerType, accountScope);
+
+        switch (providerType) {
+            case AWS:
+                String accessKey = credentials.get("accessKeyId");
+                String secretKey = credentials.get("secretAccessKey");
+                String region = credentials.get("region");
+                
+                return awsCredentialManager.storeCredentials(tenantKey, accessKey, secretKey, region)
+                        .getCredentialKey();
+            case AZURE:
+            case GCP:
+                throw new BusinessException(
+                    CloudErrorCode.UNSUPPORTED_OPERATION,
+                    String.format("프로바이더 타입 %s는 아직 지원되지 않습니다", providerType)
+                );
+            default:
+                throw new BusinessException(
+                    CloudErrorCode.UNSUPPORTED_OPERATION,
+                    String.format("알 수 없는 프로바이더 타입입니다: %s", providerType)
+                );
+        }
+    }
+
+    @Override
+    public void deleteCredentials(String credentialKey) {
+        log.debug("[CredentialProviderPortAdapter] deleteCredentials - credentialKey={}", credentialKey);
+        
+        // 현재는 AWS만 지원하므로 AWS 자격증명 관리자로 위임
+        // 향후 다른 프로바이더 지원 시 credentialKey prefix 등으로 구분 가능
+        awsCredentialManager.deleteCredentials(credentialKey);
+    }
+}
+
