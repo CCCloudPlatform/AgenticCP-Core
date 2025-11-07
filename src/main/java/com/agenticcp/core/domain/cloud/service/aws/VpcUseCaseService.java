691dd90bdcf0e7fea0e71e8d1eea37fa975d11bc
yeonsoo <dfh56541@gmail.com>
2025-11-07 19:38:09 +0900

    feat: VpcUseCaseService 에서 command 객체 변환 및 capabilityGuard 적용

--- src/main/java/com/agenticcp/core/domain/cloud/service/aws/VpcUseCaseService.java
+++ src/main/java/com/agenticcp/core/domain/cloud/service/aws/VpcUseCaseService.java
@@ -18,39 +18,92 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 import java.util.Optional;
 
+import com.agenticcp.core.domain.cloud.capability.CapabilityGuard;
+
 @Service
 @RequiredArgsConstructor
 public class VpcUseCaseService {
 
     private final VpcPortRouter vpcPortRouter;
+    private final CapabilityGuard capabilityGuard;
 
     @Transactional
     public CloudResource createVpc(VpcCreateRequest request) {
+        capabilityGuard.ensureSupported(request.getProviderType(), "vpc", "create", CapabilityGuard.Operation.TAGGING);
         VpcManagementPort vpcPort = vpcPortRouter.getPort(request.getProviderType());
-        return vpcPort.createVpc(request);
+        CreateVpcCommand command = CreateVpcCommand.builder()
+            .providerType(request.getProviderType())
+            .accountScope(request.getAccountScope())
+            .region(request.getRegion())
+            .vpcName(request.getVpcName())
+            .cidrBlock(request.getCidrBlock())
+            .description(request.getDescription())
+            .tags(request.getTags())
+            .tenantKey(request.getTenantKey())
+            .providerSpecificConfig(request.getProviderSpecificConfig())
+            .build();
+        return vpcPort.createVpc(command);
     }
 
     @Transactional(readOnly = true)
     public Optional<CloudResource> getVpc(ResourceIdentity vpcId) {
         VpcManagementPort vpcPort = vpcPortRouter.getPort(vpcId.getProviderType());
-        return vpcPort.getVpc(vpcId);
+        GetVpcCommand command = GetVpcCommand.builder()
+            .providerType(vpcId.getProviderType())
+            .accountScope(vpcId.getAccountScope())
+            .region(vpcId.getRegion())
+            .providerResourceId(vpcId.getProviderResourceId())
+            .serviceKey(vpcId.getServiceKey())
+            .resourceType(vpcId.getResourceType())
+            .build();
+        return vpcPort.getVpc(command);
     }
 
     @Transactional(readOnly = true)
     public List<CloudResource> listVpcs(VpcQuery query) {
         VpcManagementPort vpcPort = vpcPortRouter.getPort(query.getProviderType());
-        return vpcPort.listVpcs(query);
+        ListVpcsQuery command = ListVpcsQuery.builder()
+            .providerType(query.getProviderType())
+            .accountScope(query.getAccountScope())
+            .region(query.getRegion())
+            .vpcName(query.getVpcName())
+            .cidrBlock(query.getCidrBlock())
+            .tags(query.getTags())
+            .tenantKey(query.getTenantKey())
+            .build();
+        return vpcPort.listVpcs(command);
     }
 
     @Transactional
     public CloudResource updateVpc(ResourceIdentity vpcId, VpcUpdateRequest request) {
+        capabilityGuard.ensureSupported(vpcId.getProviderType(), "vpc", "update", CapabilityGuard.Operation.TAGGING);
         VpcManagementPort vpcPort = vpcPortRouter.getPort(vpcId.getProviderType());
-        return vpcPort.updateVpc(vpcId, request);
+        UpdateVpcCommand command = UpdateVpcCommand.builder()
+            .providerType(vpcId.getProviderType())
+            .accountScope(vpcId.getAccountScope())
+            .region(vpcId.getRegion())
+            .vpcName(vpcId.getVpcName())
+            .description(request.getDescription())
+            .tags(request.getTags())
+            .tenantKey(vpcId.getTenantKey())
+            .providerSpecificConfig(request.getProviderSpecificConfig())
+            .build();
+        return vpcPort.updateVpc(command);
     }
 
     @Transactional
     public void deleteVpc(ResourceIdentity vpcId) {
+        capabilityGuard.ensureSupported(vpcId.getProviderType(), "vpc", "delete", CapabilityGuard.Operation.TAGGING);
         VpcManagementPort vpcPort = vpcPortRouter.getPort(vpcId.getProviderType());
-        vpcPort.deleteVpc(vpcId);
+        DeleteVpcCommand command = DeleteVpcCommand.builder()
+            .providerType(vpcId.getProviderType())
+            .accountScope(vpcId.getAccountScope())
+            .region(vpcId.getRegion())
+            .providerResourceId(vpcId.getProviderResourceId())
+            .serviceKey(vpcId.getServiceKey())
+            .resourceType(vpcId.getResourceType())
+            .tenantKey(vpcId.getTenantKey())
+            .build();
+        vpcPort.deleteVpc(command);
     }
 }
