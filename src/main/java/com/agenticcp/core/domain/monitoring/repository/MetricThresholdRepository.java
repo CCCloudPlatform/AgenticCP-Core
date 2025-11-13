919690b1aa79eedc62f5021e5461e14aff2eef52
grovince <grovinceln@gmail.com>
2025-11-13 17:29:30 +0900

    refactor(monitoring): Repository JavaDoc 개선 및 코드 스타일 준수
    - 클래스 JavaDoc에 @author, @version, @since 추가
    - 모든 메서드 JavaDoc에 @param, @return 태그 추가
    - MetricRepository: @Modifying 추가 및 비표준 JPQL LIMIT 제거
    
    수정 파일:
    - AlertRepository
    - MetricRepository
    - MetricThresholdRepository
    - TenantCollectorConfigRepository
    - TenantDataRetentionPolicyRepository

--- src/main/java/com/agenticcp/core/domain/monitoring/repository/MetricThresholdRepository.java
+++ src/main/java/com/agenticcp/core/domain/monitoring/repository/MetricThresholdRepository.java
@@ -10,37 +10,57 @@ import java.util.List;
 
 /**
  * 메트릭 임계값 데이터 접근 계층
+ * 
+ * @author AgenticCP Team
+ * @version 1.0.0
+ * @since 2025-11-13
  */
 @Repository
 public interface MetricThresholdRepository extends JpaRepository<MetricThreshold, Long> {
     
     /**
      * 메트릭명으로 임계값 조회
+     * 
+     * @param metricName 메트릭 이름
+     * @return 해당 메트릭의 임계값 목록
      */
     List<MetricThreshold> findByMetricName(String metricName);
     
     /**
      * 활성화된 임계값 조회
+     * 
+     * @return 활성화된 임계값 목록
      */
     List<MetricThreshold> findByIsActiveTrue();
     
     /**
      * 메트릭명과 활성화 상태로 임계값 조회
+     * 
+     * @param metricName 메트릭 이름
+     * @return 해당 메트릭의 활성화된 임계값 목록
      */
     List<MetricThreshold> findByMetricNameAndIsActiveTrue(String metricName);
     
     /**
      * 임계값 타입별 조회
+     * 
+     * @param thresholdType 임계값 타입
+     * @return 해당 타입의 임계값 목록
      */
     List<MetricThreshold> findByThresholdType(MetricThreshold.ThresholdType thresholdType);
     
     /**
      * 알림 활성화된 임계값 조회
+     * 
+     * @return 알림이 활성화된 임계값 목록
      */
     List<MetricThreshold> findByAlertEnabledTrue();
     
     /**
      * 메트릭명과 알림 활성화 상태로 임계값 조회
+     * 
+     * @param metricName 메트릭 이름
+     * @return 해당 메트릭의 활성화되고 알림이 활성화된 임계값 목록
      */
     @Query("SELECT t FROM MetricThreshold t WHERE t.metricName = :metricName AND t.alertEnabled = true AND t.isActive = true")
     List<MetricThreshold> findActiveAlertThresholdsByMetricName(@Param("metricName") String metricName);
