79f420ad2d2a8d9b97412dac890f98d52a7fcbcd
grovince <grovinceln@gmail.com>
2025-11-13 22:30:10 +0900

    refactor(notification): 알람 도메인 DTO, Entity, Enum 코드 스타일 가이드 준수 및 테스트 리팩토링
    - DTO: NotificationRequest, NotificationResponse
      - 클래스 JavaDoc에 @author, @version, @since 태그 추가
      - 필드 JavaDoc 추가
      - NotificationRequest에 Validation 어노테이션 추가
    
    - Entity: Notification, NotificationChannelEntity
      - 클래스 JavaDoc에 @author, @version, @since 태그 추가
      - 테이블 인덱스 정의 추가
      - 필수 필드에 Validation 어노테이션 추가
      - 필드 JavaDoc 추가
      - @EqualsAndHashCode(callSuper = false) 추가
      - @Builder.Default 추가
    
    - Enum: ChannelType, NotificationPriority, NotificationStatus, NotificationType
      - 클래스 JavaDoc에 @author, @version, @since 태그 추가
      - Enum 상수에 JavaDoc 주석 추가
    
    - Test: NotificationRequestTest
      - 테스트 가이드에 맞게 리팩토링
      - @Nested 클래스로 그룹화
      - 테스트 메서드 네이밍 규칙 적용 (methodName_WhenCondition_ExpectedResult)
      - AssertJ로 변경
      - Validation 테스트 추가

--- src/main/java/com/agenticcp/core/domain/notification/enums/ChannelType.java
+++ src/main/java/com/agenticcp/core/domain/notification/enums/ChannelType.java
@@ -2,15 +2,50 @@ package com.agenticcp.core.domain.notification.enums;
 
 /**
  * 알림 채널 타입 열거형
+ * 
+ * @author AgenticCP Team
+ * @version 1.0.0
+ * @since 2025-11-13
  */
 public enum ChannelType {
+    /**
+     * 이메일
+     */
     EMAIL("이메일"),
+    
+    /**
+     * SMS
+     */
     SMS("SMS"),
+    
+    /**
+     * 푸시 알림
+     */
     PUSH("푸시"),
+    
+    /**
+     * 웹훅
+     */
     WEBHOOK("웹훅"),
+    
+    /**
+     * Slack
+     */
     SLACK("Slack"),
+    
+    /**
+     * Microsoft Teams
+     */
     TEAMS("Microsoft Teams"),
+    
+    /**
+     * Discord
+     */
     DISCORD("Discord"),
+    
+    /**
+     * 사용자 정의 채널
+     */
     CUSTOM("사용자 정의");
 
     private final String description;
