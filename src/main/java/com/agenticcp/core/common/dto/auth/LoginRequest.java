0650931b98ea56ac3d6e21d147cddcc7170e88cd
yejinj <sitting077@gmail.com>
2025-11-23 23:40:56 +0900

    style: 로그인 요청 DTO 및 JWT 필터 포맷 정리

--- src/main/java/com/agenticcp/core/common/dto/auth/LoginRequest.java
+++ src/main/java/com/agenticcp/core/common/dto/auth/LoginRequest.java
@@ -21,11 +21,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
-    
+
     @NotBlank(message = "사용자명은 필수입니다.")
     @Size(min = 2, max = 50, message = "사용자명은 2자 이상 50자 이하여야 합니다.")
     private String username;
-    
+
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 8, max = 100, message = "비밀번호는 8자 이상 100자 이하여야 합니다.")
     @Masked(type = MaskingType.PASSWORD)
