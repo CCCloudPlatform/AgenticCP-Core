104e621ece8a1a7588a24f583ac7f3be70af0eba
yeonsoo <dfh56541@gmail.com>
2025-11-24 19:35:56 +0900

    fix: AwsTenantIsolationAdapter 및 AwsCloudResourceCreator 수정

--- src/main/java/com/agenticcp/core/domain/tenant/cloud/service/resource/AwsCloudResourceCreator.java
+++ src/main/java/com/agenticcp/core/domain/tenant/cloud/service/resource/AwsCloudResourceCreator.java
@@ -13,10 +13,27 @@ import java.util.Map;
 public class AwsCloudResourceCreator implements CloudResourceCreator {
 
     @Override
+    public com.agenticcp.core.domain.tenant.cloud.CloudProviderType getCloudProviderType() {
+        return com.agenticcp.core.domain.tenant.cloud.CloudProviderType.AWS;
+    }
+
+    @Override
     public CloudResourceResult createVpc(String tenantKey, ResourceCreateRequest request, TenantIsolation.IsolationLevel level) {
         // Strategy 의 추상적 요청을 AWS 형식으로 변환
         Map<String, Object> awsMetadata = getAwsMetadata(request);
 
+        // TODO: 실제 AWS API 호출 구현
+        return null;
+    }
+
+    @Override
+    public CloudResourceResult createSubnets(String tenantKey, ResourceCreateRequest request, TenantIsolation.IsolationLevel level) {
+        // TODO: 실제 AWS API 호출 구현
+        return null;
+    }
+
+    @Override
+    public CloudResourceResult createSecurityGroups(String tenantKey, ResourceCreateRequest request, TenantIsolation.IsolationLevel level) {
         // TODO: 실제 AWS API 호출 구현
         return null;
     }
